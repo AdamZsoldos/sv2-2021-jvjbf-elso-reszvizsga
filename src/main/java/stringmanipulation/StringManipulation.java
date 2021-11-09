e91046843b58ed95206461894d1045ac5587c919
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2021-11-09 13:12:16 +0100

    Szövegmanipuláció feladat kész

---
+++ src/main/java/stringmanipulation/StringManipulation.java
@@ -0,0 +1,14 @@
+package stringmanipulation;
+
+public class StringManipulation {
+
+    String everyEvenCharacter(String s) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i += 2) {
+            result.append(s.charAt(i));
+        }
+
+        return result.toString();
+    }
+}
